216c290b63da221e4b6ad3841197c376009994aa
Abhay yadav <yadavabhay0054@gmail.com>
2026-09-03 02:24:29 +0530

    Time: 3 ms (100%), Space: 175.5 MB (57.94%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+       
+        int n = nums.length;
+         int ans[] = new int[n];
+        int posind = 0; int negind= 1;
+        for(int i = 0;i< n ; i++){
+            if(nums[i] < 0){
+                ans[negind] = nums[i];
+                negind+= 2;
+
+            }else{
+                ans[posind] = nums[i];
+                posind += 2;
+            }
+        }
+        return ans;
+    }
+}
